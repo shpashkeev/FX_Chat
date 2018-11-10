42920b4a1c565864f46a6d79dac0442c6932fcdb
shpashkeev <sanchez.pashkeson@gmail.com>
2018-11-10 20:18:20 +0300

    added simple secure chat logic

---
+++ server/src/app/netty/server/SecureChatServer.java
@@ -0,0 +1,54 @@
+package app.netty.server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.SslProvider;
+import io.netty.handler.ssl.util.SelfSignedCertificate;
+
+public class SecureChatServer {
+	
+	private final int port;
+	
+	private SecureChatServer(int port) {
+		this.port = port;
+	}
+	
+    public static void main(String[] args) throws Exception {
+		if( args.length != 1 ) {
+			System.err.println("usage: java EchoServer port");
+			System.exit(1);
+		}
+		
+		int port = Integer.parseInt(args[0]);
+		new SecureChatServer(port).start();
+    }
+    
+	private void start() throws Exception {
+		
+		SelfSignedCertificate ssc = new SelfSignedCertificate();
+        SslContext sslCtx = SslContextBuilder.forServer(ssc.certificate(), ssc.privateKey())
+        	.sslProvider(SslProvider.OPENSSL)
+            .build();
+
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+             .channel(NioServerSocketChannel.class)
+             .handler(new LoggingHandler(LogLevel.INFO))
+             .childHandler(new SecureChatServerInitializer(sslCtx));
+
+            b.bind(port).sync().channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+	}
+}
