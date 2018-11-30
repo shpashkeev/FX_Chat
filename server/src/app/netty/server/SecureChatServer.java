b94822ef5a797ef590e980692bea230b30225cbc
shpashkeev <sanchez.pashkeson@gmail.com>
2018-12-01 00:42:44 +0300

    added minor features and refactoring

--- server/src/app/netty/server/SecureChatServer.java
+++ server/src/app/netty/server/SecureChatServer.java
@@ -11,41 +11,40 @@ import io.netty.handler.ssl.SslContextBuilder;
 import io.netty.handler.ssl.util.SelfSignedCertificate;
 
 public class SecureChatServer {
-	
+
 	private final int port;
-	
+
 	private SecureChatServer(int port) {
 		this.port = port;
 	}
-	
-    public static void main(String[] args) throws Exception {
-		if( args.length != 1 ) {
-			System.err.println("usage: java EchoServer port");
+
+	public static void main(String[] args) throws Exception {
+		if (args.length != 1) {
+			System.err.println("Wrong args. Check for PORT number!");
 			System.exit(1);
 		}
-		
+
 		int port = Integer.parseInt(args[0]);
 		new SecureChatServer(port).start();
-    }
-    
+	}
+
 	private void start() throws Exception {
-		
+
 		SelfSignedCertificate ssc = new SelfSignedCertificate();
-        SslContext sslCtx = SslContextBuilder.forServer(ssc.certificate(), ssc.privateKey()).build();
+		SslContext sslCtx = SslContextBuilder.forServer(ssc.certificate(), ssc.privateKey()).build();
 
-        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
-        EventLoopGroup workerGroup = new NioEventLoopGroup();
-        try {
-            ServerBootstrap b = new ServerBootstrap();
-            b.group(bossGroup, workerGroup)
-             .channel(NioServerSocketChannel.class)
-             .handler(new LoggingHandler(LogLevel.INFO))
-             .childHandler(new SecureChatServerInitializer(sslCtx));
+		EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+		EventLoopGroup workerGroup = new NioEventLoopGroup();
+		try {
+			ServerBootstrap b = new ServerBootstrap();
+			b.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class)
+					.handler(new LoggingHandler(LogLevel.INFO))
+					.childHandler(new SecureChatServerInitializer(sslCtx));
 
-            b.bind(port).sync().channel().closeFuture().sync();
-        } finally {
-            bossGroup.shutdownGracefully();
-            workerGroup.shutdownGracefully();
-        }
+			b.bind(port).sync().channel().closeFuture().sync();
+		} finally {
+			bossGroup.shutdownGracefully();
+			workerGroup.shutdownGracefully();
+		}
 	}
 }
--- server/src/app/netty/server/SecureChatServerInitializer.java
+++ server/src/app/netty/server/SecureChatServerInitializer.java
@@ -7,24 +7,24 @@ import io.netty.handler.ssl.SslContext;
 
 public class SecureChatServerInitializer extends ChannelInitializer<SocketChannel> {
 
-    private final SslContext sslCtx;
+	private final SslContext sslCtx;
 
-    public SecureChatServerInitializer(SslContext sslCtx) {
-        this.sslCtx = sslCtx;
-    }
+	public SecureChatServerInitializer(SslContext sslCtx) {
+		this.sslCtx = sslCtx;
+	}
 
-    @Override
-    public void initChannel(SocketChannel ch) throws Exception {
-        ChannelPipeline pipeline = ch.pipeline();
+	@Override
+	public void initChannel(SocketChannel ch) throws Exception {
+		ChannelPipeline pipeline = ch.pipeline();
 
-        // Add SSL handler first to encrypt and decrypt everything.
-        // In this example, we use a bogus certificate in the server side
-        // and accept any invalid certificates in the client side.
-        // You will need something more complicated to identify both
-        // and server in the real world.
-        pipeline.addLast(sslCtx.newHandler(ch.alloc()));
+		// Add SSL handler first to encrypt and decrypt everything.
+		// In this example, we use a bogus certificate in the server side
+		// and accept any invalid certificates in the client side.
+		// You will need something more complicated to identify both
+		// and server in the real world.
+		pipeline.addLast(sslCtx.newHandler(ch.alloc()));
 
-        // and then business logic.
-        pipeline.addLast(new SecureChatServerHandler());
-    }
+		// and then business logic.
+		pipeline.addLast(new SecureChatServerHandler());
+	}
 }
